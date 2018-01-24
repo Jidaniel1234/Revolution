47e4e0995f5b5e2e32d5e20384e788a7e8f27ed3
Daniel Ji <Jidaniel1234@gmail.com>
2018-01-23 20:12:26 -0800

    IGOTTHELOGOTOWORK

--- indedx.js
+++ indedx.js
@@ -1,9 +1,9 @@
-$(document).ready(() => {
+$(document).ready( () => {
 
-  // To print a bunch of jumbled letters
+  // To make a bunch of jumbled letters
   function makeId() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789          ";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 10; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -12,27 +12,38 @@ $(document).ready(() => {
   return text;
   };
 
-  // Print a bunch of bunch of jumbled letter and set last to the company name
-  var j = 0;
-  var madeIdArray =  [];
-  madeIdArray[15] = "R E V O L U T I O N";
+  //Create randomWordArray and HTML
 
-  while (j < 15) {
-    madeIdArray[j] = makeId();
-    i++;
+  var randomWordArray = [];
+
+  for (var j = 0; j < 15; j++) {
+    randomWordArray[j] = makeId();
   };
 
-  //Display them
-  var divs = $('div[id^="content-"]').hide(),
-    i = 0;
+// Output letters
+randomWordArray.forEach(function jumboWordDisplay(element, index) {
 
-    (function cycle() {
+      var dispIndex = index + 1;
+      $(".header").append("<div id='title-" + dispIndex + "' class='title'>" + element + "</div>");
+});
 
-    divs.eq(i).fadeIn(4)
-              .delay(100)
-              .fadeOut(4, cycle);
 
-    i = ++i % divs.length;
+  var divs = $('div[id^="title-"]'),
+    h = 0;
 
+(function cycle() {
+    divs.hide().eq(h).fadeIn(20)
+              .delay(10)
+              .fadeOut(20, cycle);
+
+               if (h < 15) {
+                 h = ++h;
+               };
 })();
+
+// Company Name Show
+
+$(".final").hide();
+$(".final").show(1100);
+
 });
