d67673018d16958bffb754fe0ff80f9dc6550ea1
Daniel Ji <Jidaniel1234@gmail.com>
2018-01-28 13:38:08 -0800

    S'more Updates 1/28

--- js/indedx.js
+++ js/indedx.js
@@ -1,6 +1,7 @@
 $(document).ready( () => {
   // Underline title on load
-  $(".title").addClass("titlef");
+  $(".title-last").hide();
+  $(".title-last").show(1000);
 
   // Nav Open Jquery
   $(".open-menu").on("click", function() {
@@ -11,10 +12,14 @@ $(document).ready( () => {
     $(".overlay").removeClass("open");
   });
 
+  $(".home").on("click", function() {
+    $(".overlay").removeClass("open");
+  });
+
   // To make a bunch of jumbled letters
   function makeId() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789     ";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
   for (var i = 0; i < 10; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -24,10 +29,9 @@ $(document).ready( () => {
   };
 
   //Create randomWordArray and HTML
-
   var randomWordArray = [];
 
-  for (var j = 0; j < 20; j++) {
+  for (var j = 0; j < 10; j++) {
     randomWordArray[j] = makeId();
   };
 
@@ -47,7 +51,7 @@ $(document).ready( () => {
                 .delay(15)
                 .fadeOut(25, cycle);
 
-                 if (h < 20) {
+                 if (h < 10) {
                    h = ++h;
                  };
   })();
